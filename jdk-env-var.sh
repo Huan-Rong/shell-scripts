e123dbac2f054b13a08b9ea8616ce797ab3dabbd
Huan-Rong <zhenghuanrong@yeah.net>
2019-01-25 14:00:41 -0500

    1.export env variable: CLASSPATH

--- jdk-env-var.sh
+++ jdk-env-var.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+
+# You should put this file into /etc/profile.d/ directory
+
 JAVA_HOME=/usr/local/oracle-jdk-8/jdk1.8.0_201
 CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar
 PATH=${JAVA_HOME}/bin:${PATH}
 
 export JAVA_HOME
+export CLASSPATH
 export PATH
