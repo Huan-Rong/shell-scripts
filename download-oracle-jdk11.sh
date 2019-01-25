0b301b6309acb65602420e2c8185820b578abbca
Huan-Rong <huanrong.maybe@gmail.com>
2019-01-25 12:16:04 +0800

    1.Add 2 shell scripts to download oracle jdk8 and jdk11

---
+++ download-oracle-jdk11.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Downloading oracle jdk11"
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz
